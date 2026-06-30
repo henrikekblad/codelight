b380c5ae03341e6bfce849c5329349c42dc9387d
Henrik Ekblad <henrik.ekblad@gmail.com>
2026-06-30 19:54:16 +0200

    chore: bump Android versionCode to 5 / versionName to 1.0.7

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId  = "se.sensnology.codelight"
         minSdk         = 26
         targetSdk      = 35
-        versionCode    = 4
-        versionName    = "1.0.6"
+        versionCode    = 5
+        versionName    = "1.0.7"
     }
 
     signingConfigs {
