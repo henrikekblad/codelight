785588f57665af2eb1a6e69bae0334263db79cc4
Henrik Ekblad <henrik.ekblad@gmail.com>
2026-07-01 11:47:10 +0200

    chore: bump version to 1.0.8

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId  = "se.sensnology.codelight"
         minSdk         = 26
         targetSdk      = 35
-        versionCode    = 5
-        versionName    = "1.0.7"
+        versionCode    = 6
+        versionName    = "1.0.8"
     }
 
     signingConfigs {
