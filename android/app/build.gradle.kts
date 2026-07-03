e15c715121f306b18fccc20d52bec7b1956ba4d7
Henrik Ekblad <henrik.ekblad@gmail.com>
2026-07-03 13:53:57 +0200

    chore: bump version to 1.0.11

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId  = "se.sensnology.codelight"
         minSdk         = 26
         targetSdk      = 35
-        versionCode    = 8
-        versionName    = "1.0.10"
+        versionCode    = 9
+        versionName    = "1.0.11"
     }
 
     signingConfigs {
