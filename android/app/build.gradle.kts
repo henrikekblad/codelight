7aa94edefc86a0c5adc317a6292bd10333f1fb23
Henrik Ekblad <henrik.ekblad@gmail.com>
2026-07-10 22:26:14 +0200

    Release 1.4.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId  = "se.sensnology.codelight"
         minSdk         = 26
         targetSdk      = 35
-        versionCode    = 17
-        versionName    = "1.4.1"
+        versionCode    = 18
+        versionName    = "1.4.2"
     }
 
     signingConfigs {
